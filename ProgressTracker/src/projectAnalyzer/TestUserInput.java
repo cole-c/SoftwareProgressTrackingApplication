d8d10f6dea6b2abaa234bbadd3d98982eeffc94e
ajdorenk <Austin@ajdmbpro.student.iastate.edu>
2014-10-05 23:08:25 -0500

    Created semifuncational test and updated UserInput class

--- ProgressTracker/src/projectAnalyzer/TestUserInput.java
+++ ProgressTracker/src/projectAnalyzer/TestUserInput.java
@@ -4,7 +4,27 @@ public class TestUserInput {
 
 	public static void main(String[] args) {
 		UserInput ui = new UserInput();
-		System.out.println("This test works");
+		System.out.println("Now testing UserInput module\n");
+		int numDevs = 4;
+		System.out.println("Number of developers: " + numDevs);
+		int loc = 3500;
+		System.out.println("Number of lines of code: " + loc);
+		int numMeth = 39;
+		System.out.println("Number of methods:: " + numMeth);
+		int skillOfDevs = 7;
+		System.out.println("Skill of developers (out of 10): " +skillOfDevs);
+		System.out.println("Please wait....");
+		ui.projectAnalyzer(numDevs, loc, numMeth, skillOfDevs);
+		System.out.println("Estimated complexity: " + ui.getEstimatedComplexity());
+		System.out.println("Estimated number of weeks: " + ui.getEstimatedNumberOfWeeks());
+		int expectedNumWeeks = 14;
+		if (Math.abs(expectedNumWeeks - ui.getEstimatedNumberOfWeeks()) < 3) {
+			System.out.println("\n**** SUCCESS - estimate is within expected range ****\n");
+		} else {
+			System.out.println("\n**** FAILURE - estimate exceeds expeced range ****\n");
+		}
+
+		System.out.println("UserInput module testing now complete");
 	}
 	
 }
--- ProgressTracker/src/projectAnalyzer/UserInput.java
+++ ProgressTracker/src/projectAnalyzer/UserInput.java
@@ -4,20 +4,22 @@ import java.util.Random;;
 public class UserInput {
 	
 	private int numberOfDevelopers = 0;
+	private int skillOfDevelopers = 0;
 	private int linesOfCode = 0;
 	private int numberOfMethods = 0;
 	private int estimatedNumberOfWeeks = 0;
 	private int estimatedComplexity = 0;
 	
-	public void projectAnalyzer(int numberOfDevelopers, int linesOfCode, int numberOfMethods) {
+	public void projectAnalyzer(int numberOfDevelopers, int linesOfCode, int numberOfMethods, int skillOfDevs) {
 		this.numberOfDevelopers = numberOfDevelopers;
 		this.linesOfCode = linesOfCode;
 		this.numberOfMethods = numberOfMethods;
+		this.skillOfDevelopers = skillOfDevs;
 		
-		Random rand = new Random();
-		this.estimatedComplexity = rand.nextInt() % 5;
-		
-		this.estimatedNumberOfWeeks = this.estimatedComplexity * (linesOfCode / numberOfDevelopers);
+//		Random rand = new Random();
+//		this.estimatedComplexity = rand.nextInt() % 5;
+		this.estimatedComplexity = (int) Math.round(linesOfCode/3500.0 * numberOfMethods/2.0);
+		this.estimatedNumberOfWeeks = (int) Math.round(this.estimatedComplexity * ((linesOfCode * 1.0) / (numberOfDevelopers * skillOfDevs * 175.0)));
 	}
 	
 	public int getNumberOfDevelopers() {
@@ -49,6 +51,14 @@ public class UserInput {
 	}
 	public void setEstimatedComplexity(int estimatedComplexity) {
 		this.estimatedComplexity = estimatedComplexity;
+	}
+
+	public int getSkillOfDevelopers() {
+		return skillOfDevelopers;
+	}
+
+	public void setSkillOfDevelopers(int skillOfDevelopers) {
+		this.skillOfDevelopers = skillOfDevelopers;
 	}
 	
 
